1dd3e9483b1f78d64815ca56c12f44f1b97ba309
Alex Huk <alex.huk85@gmail.com>
2019-07-21 21:16:20 +1000

    Add difflib - get_close_matches

--- english_dictionary.py
+++ english_dictionary.py
@@ -1,21 +1,23 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open('data.json'))
 
-def translate(word):
-    return data[word]
-
-word = input('Enter a word: ')
-word = word.lower()
-
-while word != 'quit_program':
-    if word not in data:
-        print('Please check your word again')
-        word = input('Enter a word: ')
-        word = word.lower()
-        continue
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yes_no = input('Did you mean %s instead? Y if yes, or N if no: ' % get_close_matches(w, data.keys())[0])
+        if yes_no == 'Y':
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yes_no == 'N':
+            return "The word doesn't exist. Please check and try again."
+        else:
+            return 'We cannot understand this word.'
     else:
-        print(translate(word))
-        word = input('Enter a word: ')
-        word = word.lower()
-print('Program ended')
+        return "The word doesn't exist. Please check and try again."
+
+user_input = input('Enter a word: ')
+
+print(translate(user_input))
